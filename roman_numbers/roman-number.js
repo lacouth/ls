8bb37dc48a6afc5bf81d61b48b1248726567f26d
Patric Lacouth <lacouth@gmail.com>
2019-09-15 20:12:32 -0300

    add roman numbers

---
+++ roman_numbers/roman-number.js
@@ -0,0 +1,18 @@
+function toRoman(number) {
+  // TODO
+  let roman = {1:'I', 
+               4:'IV',
+               5:'V',
+               9:'IX',
+               10:'X'}
+  let r = ""
+  
+  for (const b of roman.reverse()) {
+    let div = 
+  }
+
+
+  return r
+}
+
+export { toRoman };
