cfc97e49b0e30669fc2e9c9e1b0978811648eb70
ifpb <ifpb@MDK17LB02-JP.ad.ifpb.edu.br>
2019-10-04 15:35:43 -0300

    New socks

--- extra/socks/socks.js
+++ extra/socks/socks.js
@@ -1,17 +1,21 @@
-function socksCount(socks){
-    let count = 0;
-    
-    for(let i=0; i<socks.length;i++){
-        for(let j=i+1;j<socks.length;j++){
-            if(socks[i]==socks[j]){
-                count++
-                socks.splice(j,1)
-                break
-            }
-        }
-    }
-    return count
 
+function socksCount(socks){
+    // let count = 0;
+    
+    // for(let i=0; i<socks.length;i++){
+    //     for(let j=i+1;j<socks.length;j++){
+    //         if(socks[i]==socks[j]){
+    //             count++
+    //             socks.splice(j,1)
+    //             break
+    //         }
+    //     }
+    // }
+    // return count
+
+    let result = [...new Set(socks)]
+    return result.map( v => parseInt(socks.reduce((acc,s)=> s==v ? ++acc : acc,0) / 2))
+                 .reduce((acc,v)=>acc+v,0)
     
 }
 
