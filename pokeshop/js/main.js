1ee9c172be56b7dde7fd950c7849f645bdca6435
Patric Lacouth <lacouth@gmail.com>
2019-12-06 11:13:25 -0300

    fix chevron

--- pokeshop/js/main.js
+++ pokeshop/js/main.js
@@ -502,7 +502,7 @@ const pokemons = [
   ]
 
   const pokediv = document.querySelector('#pokemons')
-  const chevrondiv = document.querySelector('#chevron')
+  const chevrondiv = document.querySelector('.nav-top')
   const cart_count = document.querySelector('.cart-count')
   let count = 0
 
@@ -519,14 +519,9 @@ const pokemons = [
 
   document.addEventListener('scroll',()=>{
     if( window.scrollY > window.innerHeight/3 ){
-        chevrondiv.innerHTML = 
-        `<div class="nav-top">
-            <svg class="fa-chevron-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
-            </svg>
-        </div>`
+        chevrondiv.className = 'nav-top'
     }else{
-        chevrondiv.innerHTML = ''
+        chevrondiv.className = 'nav-top hidden'
     }
   })
 
